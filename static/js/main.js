b2d8ea291e04e1f82f3471135da329e5a6bd64aa
Alexander Hogue <defaultnamehere@gmail.com>
2014-07-10 12:22:24 +1000

    removed js onload

--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,34 @@
-$.ready(function() { 
-    var faces= $("button.facebtn");
+var faces= $("button.facebtn");
 
-    faces.popover({
-        content: "Copied to clipboard!",
-        placement: "right"
-    });
-
-    var clip = new ZeroClipboard(faces, {moviePath : "../static/js/zeroclipboard/ZeroClipboard.swf"});
-
-    clip.glue(faces);
-
-    clip.on("mousedown", function(client, args) {
-        $(this).popover('toggle');
-        var id = $(this).attr("face-id")
-        $.ajax("/increment?id=" + id, {
-            type: "POST"
-            });
-    });
-
-    clip.on("mouseout", function(client, args) {
-        $(this).popover('hide');
-        $(this).css('background-color', '');
-        $(this).css('color', '#333333');
-        $(this).addClass('bg-img');
-    });
-
-    clip.on("mouseover", function(client, args) {
-        $(this).removeClass('bg-img');
-        $(this).css('background-color', '#408FFF');
-        $(this).css('color', '#FFFFFF');
-    });
-
-    // Deal with bookmark cookie.
-     
-
+faces.popover({
+    content: "Copied to clipboard!",
+    placement: "right"
 });
+
+var clip = new ZeroClipboard(faces, {moviePath : "../static/js/zeroclipboard/ZeroClipboard.swf"});
+
+clip.glue(faces);
+
+clip.on("mousedown", function(client, args) {
+    $(this).popover('toggle');
+    var id = $(this).attr("face-id")
+    $.ajax("/increment?id=" + id, {
+        type: "POST"
+        });
+});
+
+clip.on("mouseout", function(client, args) {
+    $(this).popover('hide');
+    $(this).css('background-color', '');
+    $(this).css('color', '#333333');
+    $(this).addClass('bg-img');
+});
+
+clip.on("mouseover", function(client, args) {
+    $(this).removeClass('bg-img');
+    $(this).css('background-color', '#408FFF');
+    $(this).css('color', '#FFFFFF');
+});
+
+// Deal with bookmark cookie.
+
