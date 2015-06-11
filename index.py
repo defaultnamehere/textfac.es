65bcc57e5e174263c9a3d052191443c931746b41
Alexander Hogue <defaultnamehere@gmail.com>
2015-06-11 21:35:18 +1000

    Whoops index.py nice feature launch Alex

--- index.py
+++ index.py
@@ -3,5 +3,3 @@ import os
 sys.path.append('/var/sites/textfac.es')
 logging.basicConfig(stream=sys.stderr)
 from textface import app as application
-if os.environ["TEXTFACES_ENV"] == "DEV":
-    application.run(debug=True)
