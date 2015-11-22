ecca9514fb48b2776dcd79605045170dd1c0a003
Alexander Hogue <defaultnamehere@gmail.com>
2015-11-22 15:43:15 +1100

    remove console.log

--- static/js/faq.js
+++ static/js/faq.js
@@ -1,7 +1,6 @@
 $(function() {
     // FAQ hackery
     $("h4.faq").click(function() {
-        console.log("clicked");
         console.log($(this).next());
         $(this).next().toggleClass("hidden");
     });
